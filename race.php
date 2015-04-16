c93435e376e0b11bd64fe0e0ee69c2849a866a04
nidhirnaik <nidhirnaik1993@gmail.com>
2015-04-16 20:16:01 +0530

    integrated copy

--- race.php
+++ race.php
@@ -8,22 +8,82 @@
     <script src="js/vendor/modernizr.js"></script>
   </head>
   <body>
+   <nav class="top-bar" data-topbar role="navigation">
+  <ul class="title-area">
+    <li class="name">
+   <!--<h1><a href="#">My Site</a></h1>
+    </li>-->
+     <!-- Remove the class "menu-icon" to get rid of menu icon. Take out "Menu" to just have icon alone -->
+    <li class="toggle-topbar menu-icon"><a href="#"><span>Menu</span></a></li>
+  </ul>
+
+  <section class="top-bar-section">
+    <!-- Right Nav Section -->
+    <!--<ul class="right">
+      <li class="active"><a href="#">Right Button Active</a></li>
+      <li class="has-dropdown">
+        <a href="#">Right Button Dropdown</a>
+        <ul class="dropdown">
+          <li><a href="#">First link in dropdown</a></li>
+          <li class="active"><a href="#">Active link in dropdown</a></li>
+        </ul>
+      </li>
+    </ul>!-->
+
+    <!-- Left Nav Section -->
+   <ul class="left">
+      <li style="font-weight: bold;"><a href="#">TURF CLUB</a></li>
+    </ul>
+    
+    <!--If session, display this -->
+    <ul class="right">
+         <li class="has-dropdown not-click">
+         <?php echo "<a href='#'>".$name."</a>"; ?>
+         <ul class="dropdown">
+         <li><a href="#">Sign out</a></li>
+    </ul>
+        </ul> <!-- till here -->
+        
+        <!-- Else -->
+    <ul class="right">
+      <li><a href="Forms/Form_Signup.html">SIGNUP</a></li>
+    </ul>
+   <ul class="right">
+      <li><a href="Forms/Form_Login.html">LOGIN</a></li>
+    </ul>
+    <!-- till here -->
+    
+  </section>
+</nav>
+<br>
+
     <div class="row">
         <div class="large-12 columns">
-			
-          <ul class="rectangle button-group">
-            <li><a href="#" class="button">Home</a></li>
-            <li><a href="#" class="button">About Us</a></li>
-            <li><a href="#" class="button">Racing</a>
-			</li>
-            <li><a href="#" class="button">Fixtures</a></li>
+          <ul class="round button-group">
+            <li><a href="derbyhome.html" class="button">Home</a></li>
+            <li><a href="About_Us.html" class="button">About Us</a></li>
+            <li class="has-dropdown not-click"><a href="#" class="button" data-dropdown="hover1" data-options="is_hover:true">Racing</a>
+            <ul id="hover1" class="f-dropdown" data-dropdown-content>
+                <li><a href="horse_list.php" >Horses </li>
+                <li><a href="jockey_list.php" >Jockeys </li>
+                <li><a href="trainer_list.php" >Trainers </li>
+                <li><a href="betting_rules.html" >Betting</li>
+            </ul>                        
+            </li>
+            <li  class="has-dropdown not-click"><a href="#" class="button" data-dropdown="hover2" data-options="is_hover:true">Betting</a>
+               <ul id="hover2" class="f-dropdown" data-dropdown-content>
+                <li><a href="Form_betting.php" >Win</li>
+                <li><a href="Form_betting.html" >Place</li>
+               </ul>
+             </li>
             <li><a href="#" class="button">Archives</a></li>
-            <li><a href="#" class="button">Photo Gallery</a></li>
-            <li><a href="#" class="button">Contact us</a></li>
+            <li><a href="Photo_gallery.html" class="button">Photo Gallery</a></li>
+            <li><a href="contact_us.html" class="button">Contact us</a></li>
           </ul>
-		  
          </div>
-      </div>
+      </div>          
+          
+          
 <div id="list" style="margin:20px 260px 0px 196px;">
 <?php 
 session_start();
