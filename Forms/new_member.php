50ec75be1f22998c42df7cb274e09831006ad7c1
madhuradas <das.madhura94@gmail.com>
2015-04-18 17:00:48 +0530

    cleaned up code

--- Forms/new_member.php
+++ Forms/new_member.php
@@ -4,8 +4,8 @@
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Login</title>
-    <link rel="stylesheet" href="css/foundation.css" />
-    <script src="js/vendor/modernizr.js"></script>
+    <link rel="stylesheet" href="/se-derby/css/foundation.css" />
+    <script src="/s-derby/js/vendor/modernizr.js"></script>
   </head>
   <body>
    <div class="row">
