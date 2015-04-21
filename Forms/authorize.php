5d5b1cf80cdb5a59f2894548b5b166dddbdebc4e
meghanapochiraju <pochiraju.meghana@gmail.com>
2015-04-21 20:15:59 +0530

    admin redirect for admin user

--- Forms/authorize.php
+++ Forms/authorize.php
@@ -9,7 +9,7 @@ $value = false;
 $email = $_POST['email'];		
 $password = $_POST['pwd'];
 $client = $_POST['client'];	
-
+$no = 0;
 	
 $conn = mysqli_connect($dbhost, $dbuser, $dbpass,$db);
 if (mysqli_connect_errno())
@@ -57,8 +57,16 @@ $row1=mysqli_fetch_assoc($result1);
 if (password_verify($password, $pwd)){//$password == $pwd) {
    echo '<br>Password is valid!';
     $value =true;
+    if ($email == 'admin' and $password == 'admin')
+    {
+         $no = 2;
+    }
+    else
+      $no = 1;
     echo '<script type = "text/javascript"> alert("login Successful");</script>' ;
-} else {
+}
+
+ else {
   $value= false;
   echo '<script type = "text/javascript"> alert("login failed");</script>' ;
 }
@@ -78,11 +86,14 @@ else if ($client=='web')
 {
 
 if($value){
-  echo "in if";
-  header('Location:/se-derby/derbyhome.php');
+  //echo "in if";
+  if($no == 1)
+    header('Location:/se-derby/derbyhome.php');
+  else if($no == 2)
+    header('Location:/se-derby/admin_home.html');
 }
  else if(!$value){
- echo "in else";
+ //echo "in else";
  header('Location:Form_Login.html');
 }
 }
