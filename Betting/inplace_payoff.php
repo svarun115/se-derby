6e9442a25958988689107252536355bd5e7e5dca
meghanapochiraju <pochiraju.meghana@gmail.com>
2015-04-25 09:21:05 +0530

    payoff for inplace betting

--- Betting/inplace_payoff.php
+++ Betting/inplace_payoff.php
@@ -1,40 +1,48 @@
 <?php
-echo "in second file";
+//echo "in second file";
+session_start();
 $dbhost = 'localhost';
 $db2='derby';
 $dbuser = 'admin';
 $dbpass = 'admin';
+$db1='club';
 $conn = mysqli_connect($dbhost,$dbuser,$dbpass,$db2);
-
+$conn1 = mysqli_connect($dbhost,$dbuser,$dbpass,$db1);
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
+if (!$conn1) {
+    die("Connection failed: " . mysqli_connect_error());
+}
 
-echo "sql part";
-
+//echo "sql part";
+$race = $_SESSION["race"];
+$table_name= $race."_odds_place";
 $winner_first="Lord Trondor";//change
 $winner_second="Market Outlook";//change
 $payoff=0;
 $profit_first=0;
 $profit_second=0;
-$sql="select * from place";
-$sql1="select * from odds_place";
-
+$sql="SELECT * from place WHERE race_name = '".$race."';";
+$sql1="SELECT * from $table_name";
+$odds=array();
 $result=mysqli_query($conn,$sql);
+if(!$result)
+	echo $conn->error;
 $result2=mysqli_query($conn,$sql1);
-echo "<br>";
+//echo "<br>";
 
 while($row1=mysqli_fetch_assoc($result2))
 			{
-					$odds[$row1["horse_name"]]=$row1["odd"];
-					echo $row1["horse_name"]."=".$row1["odd"]."<br>";
+					$odds[$row1["horse_name"]]=$row1["odds_fraction"];
+					//echo $row1["horse_name"]."=".$row1["odds_fraction"]."<br>";
 			}
-foreach ($odds as $value) {
+/*foreach ($odds as $value) {
 	# code...
 	echo "value=".$value."<br>";
-}
-echo "<br><br>";
-echo "first:".$winner_first."<br>second:".$winner_second."<br><br>";
+}*/
+//echo "<br><br>";
+//echo "first:".$winner_first."<br>second:".$winner_second."<br><br>";
 if (mysqli_num_rows($result) > 0) {
     // output data of each row
     while($row = mysqli_fetch_assoc($result))
@@ -46,12 +54,12 @@ if (mysqli_num_rows($result) > 0) {
 		if($row["horse_name_place1"]==$winner_first)
 		{
 			
-			$profit_first=$odds[$winner_first]*($row["amount"])*0.6;
+			$profit_first=$odds[$row["horse_name_place1"]]*($row["amount"])*0.6;
 		}
 		if($row["horse_name_place2"]==$winner_second)
 		{
 			
-			$profit_second=$odds[$winner_second]*($row["amount"])*0.4;
+			$profit_second=$odds[$row["horse_name_place2"]]*($row["amount"])*0.4;
 		}
 		if($profit_first==0 and $profit_second==0)
 		{
@@ -61,7 +69,14 @@ if (mysqli_num_rows($result) > 0) {
 		{
 			$payoff=$row["amount"]+$profit_first+$profit_second;
 		}
-		echo "member-".$row["member_id"]."profit-".($profit_first+$profit_second)."payoff-".$payoff."<br>";   
+		echo "member-".$row["member_id"]." profit-".($profit_first+$profit_second)." payoff-".$payoff."<br>"; 
+		if($payoff>0){
+			$member_id= $row["member_id"];
+		$sql3 = "UPDATE club.account SET balance = balance-$payoff where member_id=$member_id;" ;
+		$result3 = mysqli_query($conn1,$sql3);
+		if(!$result3)
+			echo $conn1->error;
+		}
     }
 } else {
     echo "0 results";
